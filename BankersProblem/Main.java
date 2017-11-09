47412cd478258cf8f5de9f4e40f595287a70e41b
lmreller <31704165+lmreller@users.noreply.github.com>
2017-11-09 10:48:07 -0600

    added input validation

--- BankersProblem/Main.java
+++ BankersProblem/Main.java
@@ -3,23 +3,33 @@ import java.util.Scanner;
 
 /*
 This class will run the program as well as handle all I/O with the user
-*/
-public class Main{
-    public static void main(String args[]){
+ */
+public class Main {
+
+    public static void main(String args[]) {
         int inputResources;
         int inputClients;
-        
+
         Scanner scan = new Scanner(System.in);
         String input;
-        
-        System.out.print("Please input the number of bank resources: ");
-        input = scan.nextLine();
-        inputResources = Integer.parseInt(input);     
-        System.out.println("");
-        
-        System.out.print("Please input the number of clients: ");
-        input = scan.nextLine();
-        inputClients = Integer.parseInt(input);
-        System.out.println("");
+        Boolean isValid = false;
+        while (!isValid) {
+            System.out.print("Please input the number of bank resources (between 1-10): ");
+            input = scan.nextLine();
+            inputResources = Integer.parseInt(input);
+            System.out.println("");
+
+            System.out.print("Please input the number of clients (between 1-10): ");
+            input = scan.nextLine();
+            inputClients = Integer.parseInt(input);
+            System.out.println("");
+
+            if ((inputResources > 0 && inputResources <= 10) && (inputClients > 0 && inputClients <= 10)) {
+                isValid = true;
+            }
+            else{
+                System.out.println("Invalid input, please input values between 1 and 10");
+            }
+        }
     }
 }
